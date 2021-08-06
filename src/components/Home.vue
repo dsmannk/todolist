da347ddfbaecf3939d79cb335b94539ca4783762
dsmannk <dsmannk@naver.com>
2021-08-07 00:21:59 +0900

    [20210807_dsmannk]todolist만들기

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid grid-list-sm pa-0>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <List :todoList="todoList"></List>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <ListAdd @listAdd="listAdd"></ListAdd>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import List from "./List";
+import ListAdd from "./ListAdd";
+
+export default {
+  components: {
+    List,
+    ListAdd,
+  },
+  data: () => ({
+    todoList: [],
+  }),
+  methods: {
+    listAdd(memo) {
+      console.log("받았어");
+      this.todoList.push({ memo: memo, status: "created" });
+    },
+  },
+};
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>투두리스트</div>
+</template>
+
+<script>
+export default {
+  props: ["todoList"],
+};
+</script>
